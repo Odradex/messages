44b2f9869a46a83f5e6181aee872c35ba47b3e7b
Odradex <sivakov.daniil@gmail.com>
2024-04-11 14:08:00 +0300

    Add repo and service

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,28 @@
+import { read } from "fs"
+import { readFile, writeFile } from "fs/promises"
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const messages = await this.readMessagesFromFile()
+
+    return messages[id]
+  }
+
+  async findAll() {
+    return await this.readMessagesFromFile()
+  }
+
+  async create(content: string) {
+    const messages = await this.readMessagesFromFile()
+    const id = Math.floor(Math.random() * 999)
+
+    messages[id] = { id, content }
+
+    await writeFile('messages.json', JSON.stringify(messages))
+  }
+  
+  private async readMessagesFromFile() {
+    const contents = await readFile('messages.json', 'utf-8')
+    return JSON.parse(contents)
+  }
+}
